b3e994263be981fedab2f8ee0c94dcab1b59ca02
dinushchathurya <dinushchathurya21@gmail.com>
2022-01-16 15:01:36 +0530

    refactor: Change application scope

--- src/cat/cat.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CatService } from './cat.service';
-import { CatResolver } from './cat.resolver';
-
-@Module({
-  providers: [CatService, CatResolver]
-})
-export class CatModule {}
